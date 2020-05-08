00d9595d1ad630e69535ad314f67c86bb40f4b25
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-05-08 00:32:17 -0400

    Test map

---
+++ test_map.js
@@ -0,0 +1,32 @@
+//Map takes (Lat,Lng)
+var mymap = L.map('mapid').setView([40.631229, -73.952558], 19);
+
+// Credit OpenStreetMap(OSM)
+const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+//Tiling is basically the skin of the map
+//In the case we're using the default OSM and storing it in tileURL
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+//tilerLayer takes in URL and object
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+
+//Adds marker to map
+var marker = L.marker([40.631229, -73.952558]).addTo(mymap);
+var marker2 = L.marker([40.631135, -73.95336]).addTo(mymap);
+
+//Adds circle to map
+var circle = L.circle([40.630754, -73.95147], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 30
+}).addTo(mymap);
+
+//Marker popup
+marker.bindPopup("<b>Bedford Avenue</b><br>Cars go vroom vroom here").openPopup();
+marker2.bindPopup("<b>This is called the Quad or West Quad</b><br>But nobody calls the other side East Quad<br>Like I never heard anyone call it")
+
+//Circle popup
+circle.bindPopup("This is not Boylan");
